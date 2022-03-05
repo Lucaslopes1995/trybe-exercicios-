172fcf94b693df7c85eb5d66b8d50e103528de02
Lucas <lucaslopes@gea.inatel.br>
2022-03-05 16:41:40 -0300

    Exercicios completos aula 6.1

---
+++ fundamentos/bloco6/aula1/exercicios/script.js
@@ -0,0 +1,24 @@
+let botaoEnviar = document.querySelector("#botaoEnviar");
+let inputImagem = document.querySelector("#checkbox2");
+let inputNome = document.querySelector("#name");
+let inputEmail = document.querySelector("#email");
+
+botaoEnviar.addEventListener('click',function cancelaEnvio(){
+    if(inputImagem.checked){
+        validadorcaracteres()
+    }else{
+        event.preventDefault();
+    }
+
+})
+
+function validadorcaracteres (){
+    console.log(inputNome)
+    if(inputNome.value.length<=10 || inputNome.value.length>=40 ){
+        alert("O número de Caracteres do Nome precisa estar entre 10 e 40 Caracteres")
+    }else if(inputEmail.value.length<=10 || inputEmail.value.length>=40 ){
+        alert("O número de Caracteres do Email precisa estar entre 10 e 50 Caracteres")
+    }else{
+        alert("Dados Enviados com Sucesso")
+    }
+}
